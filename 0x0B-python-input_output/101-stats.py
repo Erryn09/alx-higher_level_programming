973567ca2e9113fc49173a3cf58a2e9ce2b8ce0b
Laughter Ayo-Ola <58696371+Erryn09@users.noreply.github.com>
2022-07-06 01:02:17 +0100

    Final Commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,76 +5,49 @@ A module on log parsing
 
 import sys
 
-ln = 0
-file_size = 0
-sc20 = 0
-sc31 = 0
-sc40 = 0
-sc41 = 0
-sc43 = 0
-sc44 = 0
-sc45 = 0
-sc50 = 0
+
+def print_stat(filesize, statcode):
+    """
+    A function for printing the logs
+    """
+    print("File size: {:d}". format(filesize))
+    for k, v in sorted(statcode.items()):
+        if v > 0:
+            print("{:s}: {:d}".format(k, v))
+
+
 if __name__ == "__main__":
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
     try:
         for line in sys.stdin:
+            if count == 10:
+                print_stat(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
             try:
-                ln += 1
-                p_line = line.split(" ")
-                stat_code = int(p_line[-2])
-                file_size += int(p_line[-1])
-                if stat_code == 200:
-                    sc20 += 1
-                elif stat_code == 301:
-                    sc31 += 1
-                elif stat_code == 400:
-                    sc40 += 1
-                elif stat_code == 401:
-                    sc41 += 1
-                elif stat_code == 403:
-                    sc43 += 1
-                elif stat_code == 404:
-                    sc44 += 1
-                elif stat_code == 405:
-                    sc45 += 1
-                elif stat_code == 500:
-                    sc50 += 1
-                if (ln % 10 == 0):
-                    print("File size: {:d}". format(file_size))
-                    if sc20 > 0:
-                        print("200: {:d}".format(sc20))
-                    if sc31 > 0:
-                        print("301: {:d}".format(sc31))
-                    if sc40 > 0:
-                        print("400: {:d}".format(sc40))
-                    if sc41 > 0:
-                        print("401: {:d}".format(sc41))
-                    if sc43 > 0:
-                        print("403: {:d}".format(sc43))
-                    if sc44 > 0:
-                        print("404: {:d}".format(sc44))
-                    if sc45 > 0:
-                        print("405: {:d}".format(sc45))
-                    if sc50 > 0:
-                        print("500: {:d}".format(sc50))
-            except KeyboardInterrupt:
+                size += int(line[-1])
+            except (IndexError, ValueError):
                 pass
 
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stat(size, status_codes)
+
     except KeyboardInterrupt:
-        print("File size: {:d}". format(file_size))
-        if sc20 > 0:
-            print("200: {:d}".format(sc20))
-        if sc31 > 0:
-            print("301: {:d}".format(sc31))
-        if sc40 > 0:
-            print("400: {:d}".format(sc40))
-        if sc41 > 0:
-            print("401: {:d}".format(sc41))
-        if sc43 > 0:
-            print("403: {:d}".format(sc43))
-        if sc44 > 0:
-            print("404: {:d}".format(sc44))
-        if sc45 > 0:
-            print("405: {:d}".format(sc45))
-        if sc50 > 0:
-            print("500: {:d}".format(sc50))
+        print_stat(size, status_codes)
+        raise
